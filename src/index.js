aa364da44974b5aa545099949b23f24970e3816c
tosha504 <tosha.rovniy@gmail.com>
2021-07-18 19:11:23 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function($) {
+  const $burger = $(".header__burger");
+
+  $burger.on("click", function() {
+    $burger.toggleClass("active");
+    $(".header__links").toggleClass("active");
+  });
+
+  $(".main-screen__slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    speed: 500,
+    cssEase: "linear",
+  });
+  // $(".slick-prev").addClass("active");
+  $(".slick-arrow").on("click", () => {
+    $(".slick-prev,.slick-next").toggleClass("active");
+  });
+
+  $(".slick-prev").html("<");
+  $(".slick-next").html(">");
+
+  $(".think__slider").slick({
+    arrows: false,
+    infinite: true,
+    dots: true,
+    cssEase: "linear",
+    // autoplay: true,
+    // autoplaySpeed: 3000,
+  });
+
+  $(".slick-dots li").html("");
+});
